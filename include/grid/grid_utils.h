8fb721e1bff116c9c04717d99f193a43597439a8
Michael Tao <mtao@dgp.toronto.edu>
2015-10-06 15:43:11 -0400

    adding interp to utils for grid

--- include/grid/grid_utils.h
+++ include/grid/grid_utils.h
@@ -72,4 +72,98 @@ namespace mtao {
         }
 }
 
+
+
+
+
+namespace mtao {
+    namespace interp_internal {
+        template <typename T, typename U>
+            T lerp(const T& a, const T& b, U alpha) {//alpha = 0 -> a, alpha = 1 -> b
+                return (1-alpha) * a + alpha * b;
+            }
+        template <typename T, typename U>
+            T bilerp(const T& a, const T& b, const T& c, const T& d, U alpha, U beta) {
+                return lerp(lerp(a,b,alpha), lerp(c,d,alpha),beta);// 0,0 => a, 0,1 => b, 1,0 => c, 1,1 => d
+            }
+        template <typename T, typename U>
+            T trilerp(const T& a, const T& b, const T& c, const T& d, const T& e, const T& f, const T& g, const T& h, U alpha, U beta, U gamma) {
+                return lerp(
+                        bilerp(a,b,c,d,alpha,beta)// 0,0,0 => a, 0,1,0 => b, 1,0,0 => c, 1,1,0 => d
+                        ,
+                        bilerp(e,f,g,h,alpha,beta)// 0,0,1 => e, 0,1,1 => f, 1,0,1 => g, 1,1,1 => h
+                        ,
+                        gamma);
+            }
+
+        template <typename GridType, typename U>
+            typename GridType::Scalar bilerp(const GridType& g, int i, int j, U alpha, U beta) {
+                return bilerp(g(i,j),g(i+1,j  ),g(i,j+1  ),g(i+1,j+1),alpha,beta);
+            }
+        template <typename GridType, typename U>
+            typename GridType::Scalar trilerp(const GridType& g, int i, int j, int k, U alpha, U beta, U gamma) {
+                return trilerp(
+                        g(i,j,k  ),g(i+1,j  ,k  ),g(i,j+1  ,k  ),g(i+1,j+1,k  )
+                        ,
+                        g(i,j,k+1),g(i+1,j  ,k+1),g(i,j+1  ,k+1),g(i+1,j+1,k+1)
+                        ,alpha,beta,gamma);
+            }
+    }
+
+    template <typename T>
+        void barycentric(T a, int ni, int* i, T* di ) {
+            a = std::min<T>(std::max<T>(a,0),1-1e-5);
+            T v = a * (ni);
+            T v2 = std::floor(v);
+            *di = v-v2;
+            *i = int(v2);
+            if(*i < 0) {
+                *di = 0;
+                *i = 0;
+            } else if(*i >= ni) {
+                *i = ni-1;
+                *di = 1-1e-9;
+            }
+        }
+
+    template <typename VecType, typename ShapeType,typename DiffType, int D = mtao::compat::tuple_size<ShapeType>::value>
+        void barycentric(const VecType& v, const ShapeType& shape, ShapeType& i, DiffType& di) {
+            static_assert(mtao::compat::tuple_size<DiffType>::value == D,"");
+            for(size_t idx = 0; idx < D; ++idx) {
+                barycentric(v(idx),shape[idx]-1,&i[idx],&di[idx]);
+            }
+        }
+    template <typename T, typename GridType>
+        typename GridType::Scalar lerp(const GridType& g, const mtao::compat::array<int,GridType::D>& i, const mtao::compat::array<T,GridType::D>& di);
+
+    template <typename T, typename GridType>
+        typename GridType::Scalar lerp(const GridType& g, const mtao::compat::array<int,2>& i, const mtao::compat::array<T,2>& di) {
+            static_assert(GridType::D == 2,"");
+            return mtao::interp_internal::bilerp(g,i[0],i[1],di[0],di[1]);
+        }
+    template <typename T, typename GridType>
+        typename GridType::Scalar lerp(const GridType& g, const mtao::compat::array<int,3>& i, const mtao::compat::array<T,3>& di) {
+            static_assert(GridType::D == 3,"");
+            return mtao::interp_internal::trilerp(g,i[0],i[1],i[2],di[0],di[1],di[2]);
+        }
+    template <typename VecType, typename GridType>
+        typename GridType::Scalar lerp(const GridType& g, const VecType& v) {
+            constexpr int Dim = GridType::D;//mtao::Grid assumption
+            //int i,j;
+            //float di,dj;
+            mtao::compat::array<int,Dim> i;
+            mtao::compat::array<typename VecType::Scalar,Dim> di;
+            barycentric(v,g.shape(),i,di);
+            return lerp(g,i,di);
+        }
+    template <typename VecType, typename GridType>
+        typename GridType::Scalar bilerp(const GridType& g, const VecType& v) {
+            return lerp(g,v);
+        }
+}
+
+
+
+
+
 #endif//GRID_UTILS_H
