fcac6e15e2a99a261603d39c75bdf0f3070556be
Michael Tao <mtao@dgp.toronto.edu>
2015-10-13 17:21:18 -0400

    static iteration seems to work without return types but with variadic inputs

--- include/static_iteration.h
+++ include/static_iteration.h
@@ -8,54 +8,55 @@ namespace mtao {
         template <typename FuncType, int D, int I,typename... Args >
             struct static_iterator: public static_iterator<FuncType,D,I+1,Args...> {
                 template <int J, typename Cont>
-                    static auto&& getN(const Cont& c) {
+                    static const typename Cont::value_type& getN(const Cont& c) {
                         return c.template get<J>();
                     }
                 template <int J, typename Cont>
-                    static auto&& getN(Cont& c) {
+                    static typename Cont::value_type& getN(Cont& c) {
                         return c.template get<J>();
                     }
 
                 //No return types
-                static void run(const FuncType& func, Args... data) {
+                static void run(const FuncType& func, Args&... data) {
                     func(getN<I>(data)...);
-                    static_iterator<FuncType,D,I+1,Args...>::run(func,std::forward<Args>(data)...);
+
+                    static_iterator<FuncType,D,I+1,Args...>::run(func,std::forward<Args&>(data)...);
                 }
-                //With return types
-                template <typename RetObj>
-                    static void run(const FuncType& func, RetObj& ret,Args... data ) {
-                        ret[I] = func(getN<I>(data)...);
-                        static_iterator<FuncType,D,I+1,Args...>::run(func,ret,std::forward<Args>(data)...);
-                    }
+                ////With return types
+                //template <typename RetObj>
+                //    static void run(const FuncType& func, RetObj& ret,Args&... data ) {
+                //        ret[I] = func(getN<I>(data)...);
+                //        static_iterator<FuncType,D,I+1,Args...>::run(func,ret,std::forward<Args&>(data)...);
+                //    }
             };
 
         template <typename FuncType, int D, typename... Args>
             struct static_iterator<FuncType,D,D,Args...> {
-                static void run(const FuncType& func,Args... data) {
+                static void run(const FuncType& func,Args&... data) {
                 }
-                template <typename RetObj>
-                    static void run(const FuncType& func, RetObj&,Args... data) {
-                    }
+                //template <typename RetObj>
+                //    static void run(const FuncType& func, RetObj&,Args... data) {
+                //    }
             };
     }
 
     template <int D, typename FuncType, typename... Args>
-        void static_loop( const FuncType& func,Args... data) {
-            internal::static_iterator<FuncType,D,0,Args...>::run(func,std::forward<Args>(data)...);
+        void static_loop( const FuncType& func,Args&... data) {
+            internal::static_iterator<FuncType,D,0,Args...>::run(func,std::forward<Args&>(data)...);
         }
-    template <int D,  typename FuncType, typename RetObj, typename... Args>
-        void static_loop(const FuncType& func, RetObj& ret,Args... data ) {
-            internal::static_iterator<FuncType,D,0,Args...>::run(func,ret,std::forward<Args>(data)...);
-        }
+    //template <int D,  typename FuncType, typename RetObj, typename... Args>
+    //    void static_loop(const FuncType& func, RetObj& ret,Args&... data ) {
+    //        internal::static_iterator<FuncType,D,0,Args...>::run(func,ret,std::forward<Args&>(data)...);
+    //    }
 
     template <int D, typename FuncType, typename... Args>
-        void const_static_loop(const FuncType& func,const Args... data) {
-            internal::static_iterator<FuncType,D,0,Args...>::run(func,std::forward<Args>(data)...);
+        void const_static_loop(const FuncType& func,Args&... data) {
+            internal::static_iterator<FuncType,D,0,Args...>::run(func,std::forward<Args&>(data)...);
         }
-    template <int D, typename FuncType, typename RetObj, typename... Args>
-        void const_static_loop(const FuncType& func, RetObj& ret,const Args... data) {
-            internal::static_iterator<FuncType,D,0,Args...>::run(func,ret,std::forward<Args>(data)...);
-        }
+    //template <int D, typename FuncType, typename RetObj, typename... Args>
+    //    void const_static_loop(const FuncType& func, RetObj& ret,Args&... data) {
+    //        internal::static_iterator<FuncType,D,0,Args...>::run(func,ret,std::forward<Args&>(data)...);
+    //    }
 
 }
 #endif//STATIC_ITERATION_H
--- src/static_iteration_test.cpp
+++ src/static_iteration_test.cpp
@@ -5,7 +5,9 @@
 
 
 struct PerDimTest {
-    PerDimTest(const std::array<int,5>& a): m_data(a) {}
+    PerDimTest(const std::array<int,5>& a): m_data(a) {
+    }
+    using value_type = int;
     template <int D>
     int& get() {
         return m_data[D];
@@ -14,17 +16,21 @@ struct PerDimTest {
     int get() const {
         return m_data[D];
     }
+    const std::array<int,5>& data() const {return m_data;}
 
     private:
         std::array<int,5> m_data;
 };
 
 int main(int argc, char * argv[]) {
-    PerDimTest a{{1,2,3,4,5}};
+    PerDimTest a{{{1,2,3,4,5}}};
     mtao::static_loop<5>([](int& b) {
             b = b * 4;
             },a);
+    std::copy(a.data().begin(),a.data().end(),std::ostream_iterator<int>(std::cout," "));
+    std::cout << std::endl;
     mtao::const_static_loop<5>([](int b) {
-            std::cout << b << std::endl;
+            std::cout << b << " ";
             },a);
+    std::cout << std::endl;
 }
