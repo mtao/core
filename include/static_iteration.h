37993d2e3381cf78555572c8aa1ffc63009b5da2
Michael Tao <mtao@dgp.toronto.edu>
2015-10-14 21:15:00 -0400

    with returns isnt complaining

--- include/static_iteration.h
+++ include/static_iteration.h
@@ -22,21 +22,21 @@ namespace mtao {
 
                     static_iterator<FuncType,D,I+1,Args...>::run(func,std::forward<Args&>(data)...);
                 }
-                ////With return types
-                //template <typename RetObj>
-                //    static void run(const FuncType& func, RetObj& ret,Args&... data ) {
-                //        ret[I] = func(getN<I>(data)...);
-                //        static_iterator<FuncType,D,I+1,Args...>::run(func,ret,std::forward<Args&>(data)...);
-                //    }
+                //With return types
+                template <typename RetObj>
+                    static void run(const FuncType& func, RetObj& ret,Args&... data ) {
+                        ret[I] = func(getN<I>(data)...);
+                        static_iterator<FuncType,D,I+1,Args...>::run(func,ret,std::forward<Args&>(data)...);
+                    }
             };
 
         template <typename FuncType, int D, typename... Args>
             struct static_iterator<FuncType,D,D,Args...> {
                 static void run(const FuncType& func,Args&... data) {
                 }
-                //template <typename RetObj>
-                //    static void run(const FuncType& func, RetObj&,Args... data) {
-                //    }
+                template <typename RetObj>
+                    static void run(const FuncType& func, RetObj&,Args&... data) {
+                    }
             };
     }
 
@@ -44,19 +44,19 @@ namespace mtao {
         void static_loop( const FuncType& func,Args&... data) {
             internal::static_iterator<FuncType,D,0,Args...>::run(func,std::forward<Args&>(data)...);
         }
-    //template <int D,  typename FuncType, typename RetObj, typename... Args>
-    //    void static_loop(const FuncType& func, RetObj& ret,Args&... data ) {
-    //        internal::static_iterator<FuncType,D,0,Args...>::run(func,ret,std::forward<Args&>(data)...);
-    //    }
+    template <int D,  typename FuncType, typename RetObj, typename... Args>
+        void static_loop_ret(const FuncType& func, RetObj& ret,Args&... data ) {
+            internal::static_iterator<FuncType,D,0,Args...>::run(func,ret,std::forward<Args&>(data)...);
+        }
 
     template <int D, typename FuncType, typename... Args>
         void const_static_loop(const FuncType& func,Args&... data) {
             internal::static_iterator<FuncType,D,0,Args...>::run(func,std::forward<Args&>(data)...);
         }
-    //template <int D, typename FuncType, typename RetObj, typename... Args>
-    //    void const_static_loop(const FuncType& func, RetObj& ret,Args&... data) {
-    //        internal::static_iterator<FuncType,D,0,Args...>::run(func,ret,std::forward<Args&>(data)...);
-    //    }
+    template <int D, typename FuncType, typename RetObj, typename... Args>
+        void const_static_loop_ret(const FuncType& func, RetObj& ret,Args&... data) {
+            internal::static_iterator<FuncType,D,0,Args...>::run(func,ret,std::forward<Args&>(data)...);
+        }
 
 }
 #endif//STATIC_ITERATION_H
