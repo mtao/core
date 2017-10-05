1c8e88dd165b0a3dfd35da032c5efa272d8fc0b0
Michael Tao <mtao@dgp.toronto.edu>
2017-10-05 15:08:01 -0400

    removed some code that was copied in to read off of and never removed

--- examples/mesh_viewer/main.cpp
+++ examples/mesh_viewer/main.cpp
@@ -12,16 +12,6 @@
 #include <glm/gtc/type_ptr.hpp> 
 
 using namespace mtao::opengl;
-glm::mat4 camera(float Translate, glm::vec2 const & Rotate)
-{
-    glm::mat4 Projection = glm::perspective(glm::radians(45.0f), 4.0f / 3.0f, 0.1f, 100.f);
-    glm::mat4 View = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -Translate));
-    View = glm::rotate(View, Rotate.y, glm::vec3(-1.0f, 0.0f, 0.0f));
-    View = glm::rotate(View, Rotate.x, glm::vec3(0.0f, 1.0f, 0.0f));
-    glm::mat4 Model = glm::scale(glm::mat4(1.0f), glm::vec3(0.5f));
-    return Projection * View * Model;
-}
-
 
 
 
