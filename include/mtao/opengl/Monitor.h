2d075db3a1e9097c8d793efc615b6a11bde4fc23
Michael Tao <mtao@dgp.toronto.edu>
2017-12-22 00:12:11 -0500

    untested monitor and frame recording code

---
+++ include/mtao/opengl/Monitor.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <GLFW/glfw3.h>
+#include <vector>
+#include <array>
+
+class Monitor {
+    public:
+        Monitor(GLFWmonitor* m = nullptr);
+        static Monitor getPrimary();
+        static std::vector<Monitor> getAll();
+
+        bool operator==(const Monitor& o) const { return monitor == o.monitor; }
+        bool operator!=(const Monitor& o) const { return monitor != o.monitor; }
+
+        const GLFWvidmode& getVideoMode();
+        std::vector<GLFWvidmode> getVideoModes();
+
+        std::array<int,2> getPos() const;
+        std::array<int,2> getPhysicalSize() const;
+        std::string getName() const;
+
+
+        GLFWmonitor* glfwMonitor() const { return monitor; }
+
+
+
+    private:
+        GLFWmonitor* monitor;
+};
--- src/opengl/renderers/renderer.cpp
+++ src/opengl/renderers/renderer.cpp
@@ -1,7 +1,4 @@
 #include "mtao/opengl/renderers/renderer.h"
-#include <vector>
-#include <png++/png.hpp>
-#include <mtao/logging/logger.hpp>
 
 namespace mtao { namespace opengl { namespace renderers {
 
@@ -35,29 +32,5 @@ namespace mtao { namespace opengl { namespace renderers {
         }
     }
 
-    void Renderer::save_frame(const std::string& filename, int w, int h) {
-        std::vector<unsigned char> data(4*w*h);
-        mtao::logging::info() << "Saving frame to disk(" << filename << "): " << w << "x" << h;
-        //glReadBuffer(GL_FRONT);
-        glReadPixels(0,0,w,h,GL_RGBA,GL_UNSIGNED_BYTE, data.data());
-        png::image<png::rgba_pixel> image(w,h);
-
-        std::cout << "Writing to image" << std::endl;
-        for (png::uint_32 y = 0; y < image.get_height(); ++y)
-        {
-            for (png::uint_32 x = 0; x < image.get_width(); ++x)
-            {
-                size_t o = 4*((h-1-y) * w+x);
-                image[y][x] = png::rgba_pixel(
-                        data[o+0],
-                        data[o+1],
-                        data[o+2],
-                        data[o+3]
-                        );
-            }
-        }
-
-        image.write(filename);
-    }
 
 }}}
