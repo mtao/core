9a08762fa8add6358ea3ca9e2dc3c115ab30887e
Michael Tao <mtao@dgp.toronto.edu>
2018-01-03 17:33:18 -0500

    adding an element filter, element_filter_vec for boolean filter vectors and element_filter for vertex constraints

---
+++ include/mtao/geometry/mesh/element_filter.hpp
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <vector>
+#include "mtao/enumerate.hpp"
+#include "mtao/types.h"
+
+
+namespace mtao { namespace geometry { namespace mesh {
+
+
+    template <typename T, int D, int D2, typename BoolVec>
+        auto element_filter_vec(const mtao::ColVectors<T,D>& V, const mtao::ColVectors<int,D2>& C, const Eigen::DenseBase<BoolVec>& B) {
+            std::vector<int> EA;
+            for(int i = 0; i < B.rows(); ++i) {
+                if(B(i)) {
+                    EA.push_back(i);
+                }
+            }
+            mtao::ColVectors<int,D2> R(C.rows(), EA.size());
+
+            for(auto&& [ni,oi]: mtao::enumerate(EA)) {
+                R.col(ni) = C.col(oi);
+            }
+            return R;
+        }
+    //For each element in C, if every vertex satisfies f(v) then it remains, tossed o.w
+    template <typename T, int D, int D2, typename Func>
+        auto element_filter(const mtao::ColVectors<T,D>& V, const mtao::ColVectors<int,D2>& C, const Func& f) {
+            mtao::VectorX<bool> B(C.cols());
+            for(int i = 0; i < C.cols(); ++i) {
+                bool enabled = true;
+                auto c = C.col(i);
+                for(int j = 0; j < C.rows(); ++j) {
+                    if(!f(V.col(c(j)))) {
+                        enabled = false;
+                    }
+                }
+                B(i) = enabled;
+            }
+            return element_filter_vec(V,C,B);
+        }
+
+
+}}}
