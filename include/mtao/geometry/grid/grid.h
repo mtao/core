3faad7f9d4b58c30f88d2cf5ace0d06edec2d86a
Michael Tao <mtao@dgp.toronto.edu>
2019-01-21 11:21:26 +0000

    updating grids a bit

--- include/mtao/geometry/grid/grid.h
+++ include/mtao/geometry/grid/grid.h
@@ -3,17 +3,16 @@
 #include "grid_utils.h"
 #include "grid_storage.h"
 #include "indexers/ordered_indexer.hpp"
+#include <Eigen/Geometry>
 
 
 namespace mtao {
     namespace geometry {
         namespace grid {
 
-            template <typename T,typename Indexer, typename Allocator = ::mtao::allocator<T>, typename StorageType = internal::grid_storage<T,Allocator>>
-                class IndexedGrid: public Indexer {
+            template <typename T, typename Indexer>
+                class Grid: public Indexer {
                     public:
-                        using storage_type = StorageType;
-                        //using storage_type = internal::grid_storage<T,Allocator>;
                         using value_type = T;
                         using Scalar = T;//for eigen compat
                         static constexpr int D = Indexer::D;
@@ -21,138 +20,73 @@ namespace mtao {
                         using Indexer::shape;
                         using Indexer::index;
                         using Indexer::width;
+                        using Vec = Vector<T,D>;
+                        using VecMap = Eigen::Map<Vec>;
+                        using CVecMap = Eigen::Map<const Vec>;
 
-                        using iterator_type = typename storage_type::iterator_type;
-                        using const_iterator_type = typename storage_type::const_iterator_type;
-                        template <typename... Args>
-                            IndexedGrid(Args... args): IndexedGrid(index_type{{static_cast<int>(args)...}}) {
-                                static_assert(sizeof...(args)== D);
-                            }
-                        IndexedGrid(const index_type& a): Indexer(a), m_storage(indexing::internal::size_from_shape(a)) {
-                            init_data();
+                        using index_scalar_type = typename index_type::value_type;
+                        using IVec = Vector<index_scalar_type,D>;
+                        using IVecMap = Eigen::Map<IVec>;
+                        using CIVecMap = Eigen::Map<const IVec>;
+
+                        static auto idx2ivec(const index_type& idx) {
+                            return CIVecMap(idx.data());
                         }
-                        IndexedGrid() {}
-                        IndexedGrid(const IndexedGrid& other) = default;
-                        //IndexedGrid(IndexedGrid&& other) = default;
-                        IndexedGrid& operator=(const IndexedGrid& other) = default;
-                        iterator_type begin() { return m_storage.begin(); }
-                        iterator_type end() { return m_storage.end(); }
-                        const_iterator_type begin() const { return m_storage.begin(); }
-                        const_iterator_type end() const { return m_storage.end(); }
+
+
+                        using BBox = Eigen::AlignedBox<T,D>;
+                        
+
+                        template <typename Derived=Vec, typename Derived2>
+                        Grid(const index_type& a, const Eigen::MatrixBase<Derived>& dx, const Eigen::MatrixBase<Derived2>& origin = Vec::Zero()): Indexer(a), m_origin(origin), m_dx(dx) {}
+                        template <typename Derived=Vec>
+                        Grid(const index_type& a, const Eigen::MatrixBase<Derived>& dx, const Vec& origin = Vec::Zero()): Indexer(a), m_origin(origin), m_dx(dx) {}
+                        Grid(const index_type& a): Grid(a,(1.0 / (CIVecMap(a.data()).template cast<T>().array()-1)).matrix()) {}
+                        Grid() {}
+                        Grid(const Grid& other) = default;
+                        //Grid(Grid&& other) = default;
+                        Grid& operator=(const Grid& other) = default;
                         void resize(const index_type& idx) {
                             Indexer::resize(idx);
-                            size_t sfs = Indexer::size();
-                            if(sfs != m_storage.size()) {
-                                m_storage.resize(sfs);
-                            }
+                        }
+
+                        BBox bbox() const {
+                            return BBox(origin(), vertex(shapeAsIVec()-IVec::Ones()));
+                        }
+
+                        template <typename Derived>
+                        Vec vertex(const Eigen::MatrixBase<Derived>& idx) const {
+                            static_assert(Derived::RowsAtCompileTime == D);
+                            static_assert(std::is_same_v<typename Derived::Scalar,index_scalar_type>);
+                            return origin() + dx().asDiagonal() * (idx.template cast<T>());
+                        }
+                        Vec vertex(const index_type& idx) const {
+                            return vertex(idx2ivec(idx));
                         }
 
 
-                        template <typename... Args>
-                            T& operator()(Args... args) {return m_storage(index(args...));}
-                        template <typename... Args>
-                            const T& operator()(Args... args) const {return m_storage(index(args...));}
-                        T& get(size_t a) {return m_storage(a);}
-                        const T& get(size_t a) const {return m_storage(a);}
-
-                        T& operator()(const index_type& t) {return m_storage(index(t));}
-                        const T& operator()(const index_type& t) const {return m_storage(index(t));}
-                        T* data() {return m_storage.data();}
-                        const T* data() const {return m_storage.data();}
-
-                        size_t size() const {return m_storage.size();}
-
-                        template <typename... Args>
-                            void set_constant(Args... args) {
-                                for(auto&& p: *this) {
-                                    p = value_type(args...);
-                                }
-                            }
-                        template <typename OpType>
-                            IndexedGrid cwiseUnaryOp(const OpType& op = OpType()) const {
-                                IndexedGrid g(shape());
-                                std::transform(begin(),end(),g.begin(),op);
-                                return g;
-                            }
-                        template <typename OpType>
-                            IndexedGrid cwiseBinaryOp(const IndexedGrid& other, const OpType& op = OpType()) const {
-                                assert(shape() == other.shape());
-                                IndexedGrid g(shape());
-                                std::transform(begin(),end(),other.begin(),g.begin(),op);
-                                return g;
-                            }
-                        IndexedGrid operator+(const IndexedGrid& other) const {
-                            return cwiseBinaryOp(other,[](auto&& a, auto&& b) {return a+b;});
+                        auto shapeAsIVec() const {
+                            return idx2ivec(shape());
                         }
-                        IndexedGrid operator-(const IndexedGrid& other) const {
-                            return cwiseBinaryOp(other,[](auto&& a, auto&& b) {return a-b;});
-                        }
-
-                        template <typename U>
-                            IndexedGrid operator*(const U& v) const {
-                                return cwiseUnaryOp([&v](auto&& a) {return v*a;});
-                            }
-                        template <typename U>
-                            IndexedGrid operator/(const U& v) const {
-                                return cwiseUnaryOp([&v](auto&& a) {return a/v;});
-                            }
-                        ////read only just over indices
-                        //void loop(const std::function<void(const index_type&)>& f) const {
-                        //    utils::multi_loop(m_shape,[&](auto&& v) {
-                        //            f(v);
-                        //            });
-                        //}
-                        //read only with values
-                        void loop(const std::function<void(const index_type&, const Scalar&)>& f) const {
-                            utils::multi_loop(shape(),[&](auto&& v) {
-                                    f(v,(*this)(v));
-                                    });
-                        }
-                        //write using index and current value
-                        void loop_write(const std::function<Scalar(const index_type&, const Scalar&)>& f) {
-                            utils::multi_loop(shape(),[&](auto&& v) {
-                                    auto&& p = (*this)(v);
-                                    p = f(v,p);
-                                    });
-                        }
-                        //write using only index
-                        void loop_write_idx(const std::function<Scalar(const index_type&)>& f) {
-                            utils::multi_loop(shape(),[&](auto&& v) {
-                                    (*this)(v) = f(v);
-                                    });
-                        }
-                        //void cwiseUnaryOp(const std::function<Scalar(const Scalar&)>& f) {
-                        //    std::transform(begin(),end(),begin(),f);
-                        //}
-
-                    public:
-                        template <typename... Args>
-                            static IndexedGrid Constant(const value_type& v,Args... args ) {
-                                IndexedGrid g(args...);
-                                g.set_constant(v);
-                                return g;
-                            }
+                        auto&& origin() const { return m_origin; }
+                        auto&& dx() const { return m_dx; }
 
                     private:
-                        void init_data() {
-                            set_constant(T{});
-                        }
-                        storage_type m_storage;
+                        Vec m_origin = Vec::Zero();
+                        Vec m_dx = Vec::Ones();
 
 
                 };
-            template <typename T, int D, typename Allocator = ::mtao::allocator<T>, typename StorageType = internal::grid_storage<T,Allocator>>
-                using Grid = IndexedGrid<T,indexing::OrderedIndexer<D>,Allocator,StorageType>;
-            /*
-               template <typename T, int D, typename Allocator = mtao::allocator<T>>
-               using DynamicGrid = Grid<T,Allocator,internal::grid_storage<T,Allocator>>;
-               template <typename T, int D, typename Allocator = mtao::allocator<T>>
-               using StaticGrid = DynamicGrid<T,D,Allocator,internal::static_storage<T>>;
-               template <typename T, int D, typename Allocator = mtao::allocator<T>>
-               using Grid = DynamicGrid<T,D,Allocator>;
-               */
 
 
+            template <typename T, int D>
+                using GridD = Grid<T,indexing::OrderedIndexer<D>>;
+            using Grid2f = GridD<float,2>;
+            using Grid3f = GridD<float,3>;
+            using Grid2i = GridD<int,2>;
+            using Grid3i = GridD<int,3>;
+            using Grid2d = GridD<double,2>;
+            using Grid3d = GridD<double,3>;
         }
 
     }
--- tests/grid_test.cpp
+++ tests/grid_test.cpp
@@ -4,173 +4,26 @@
 #include <numeric>
 
 
-template <typename T>
-void printGrid2(const T& g) {
-    for(int i = 0; i < g.template width<0>(); ++i) {
-        for(int j = 0; j < g.template width<1>(); ++j) {
-            std::cout << g(i,j) << " ";
-        }
-        std::cout << std::endl;
-    }
-}
-template <typename T>
-void printIndex2(const T& g) {
-    for(int i = 0; i < g.template width<0>(); ++i) {
-        for(int j = 0; j < g.template width<1>(); ++j) {
-            std::cout << g.index(i,j) << " ";
-        }
-        std::cout << std::endl;
-    }
-}
-
-auto returnGrid(int i, int j) {
-    mtao::geometry::grid::Grid<float,2> a(i,j);
-    for(size_t i = 0; i < a.size(); ++i) {
-        a.get(i) = i;
-    }
-    return a;
-}
-//auto returnVGrid(int i, int j) {
-//    mtao::geometry::grid::Grid<Eigen::Vector2f,2> a(i,j);
-//    for(int c = 0; c < a.size(); ++c) {
-//        a(c) = Eigen::Vector2f(float(c%i),float(j));
-//    }
-//    return a;
-//}
-struct A {
-    mtao::geometry::grid::Grid<float,2> grid;
-};
 
 
-void testA() {
-    std::cout << "TEST A" << std::endl;
-    std::cout << "======" << std::endl;
-    //mtao::geometry::grid::Grid<float,2> a(std::array<int,2>{{3,5}});
-    A a;
-    a.grid = returnGrid(3,5);
-    std::cout << "should print a 3x5 grid with inreasing indices" << std::endl;
-    printGrid2(a.grid);
-    //std::cout << std::endl;
-    //std::cout << "Retrieval!" << std::endl;
+template <int D>
+void test() {
+    using Grid = mtao::geometry::grid::GridD<float,D>;
+    typename Grid::index_type a;
+    std::iota(a.begin(),a.end(),3);
+    Grid g(a);
+    auto bb = g.bbox();
+    std::cout << bb.min().transpose() << " => " << bb.max().transpose() << std::endl;
 
-    //for(int j = 0; j < a.shape(1); ++j) {
-    //    for(int i = 0; i < a.shape(0); ++i) {
-    //        std::cout << a(i,j) << " ";
-    //    }
-    //}
-
-
-
-    //std::cout << std::endl;
-    //std::cout << std::endl;
-    ////std::cout << std::endl;
-    ////std::cout << "Creating b: =================" << std::endl;
-    //auto b = mtao::geometry::grid::Grid<Eigen::Vector2f,2>::Constant(Eigen::Vector2f::Ones(),3,5);
-    //std::cout << b.rows() << " " << b.cols() << std::endl;
-    //std::cout << "b size: " << b.size() << std::endl;
-    //printGrid2(b);
-    //std::cout << std::endl;
-    //auto c = a + b;
-    //b = a;
-
-
-    //printGrid2(b);
-    //std::cout << " == " << std::endl;
-    //printGrid2(c);
-    //printGrid2(a+c);
-    //auto d = a;
-    //printGrid2(d);
-
-    std::array<int,3> arr;
-    arr[0] = 2;
-    arr[1] = 3;
-    arr[2] = 4;
-    std::cout << "Should print out every index less than 2,3,4" << std::endl;
-    mtao::geometry::grid::utils::multi_loop(arr,[](auto&& a) {
-            for(auto&& v: a) {
-            std::cout << v << " ";
-            }
-            std::cout << std::endl;
-            });
-    std::cout << "Should print out every index less than 2,3,4, but greatest first" << std::endl;
-    mtao::geometry::grid::utils::right_multi_loop(arr,[](auto&& a) {
-            for(auto&& v: a) {
-            std::cout << v << " ";
-            }
-            std::cout << std::endl;
+    mtao::geometry::grid::utils::multi_loop(g.shape(),[&](auto&& a) {
+            std::cout << g.vertex(a).transpose() << std::endl;
             });
 
-    a.grid.loop_write_idx([&](auto&& v) -> float {
-            return v[0];
-            });
-    std::cout << "Should be a grid with just the x coordinate" << std::endl;
-    printGrid2(a.grid);
-    a.grid.loop_write_idx([&](auto&& v) -> float {
-            return v[1];
-            });
-    std::cout << "Should be a grid with just the y coordinate" << std::endl;
-    printGrid2(a.grid);
-
 
 }
 
-void testB() {
-    std::cout << "TEST B" << std::endl;
-    std::cout << "======" << std::endl;
-    auto A = returnGrid(5,5);
-    auto B = returnGrid(5,5);
-    for(auto&& a: A) {
-        a = 1;
-    }
-    for(auto&& b: B) {
-        b = 2;
-    }
-    std::cout << "Grid of just 1" << std::endl;
-    printGrid2(A);
-    std::cout << std::endl;
-    std::cout << "Grid of just 2" << std::endl;
-    printGrid2(B);
-    std::cout << std::endl;
-    mtao::geometry::grid::Grid<float,2> C;
-    C = (A-B)/3;
-    std::cout << "Grid of A-B" << std::endl;
-    printGrid2(A-B);
-    std::cout << "Grid of (A-B)/2" << std::endl;
-    printGrid2(C);
-
-}
-void matlabTest() {
-    std::cout << "MATLAB TEST:" << std::endl;
-    std::cout << "===============" << std::endl;
-    mtao::geometry::grid::Grid<int,3> g(10,11,12);
-    std::cout << "Shape:";
-    for(auto&& s: g.shape()) {
-        std::cout << s << ",";
-    }
-    std::cout << std::endl;
-    std::cout << "Size: " << g.size() << std::endl;
-    std::iota(g.begin(),g.end(),1);
-    /*
-    >> x = reshape(1:(10*11*12),[10,11,12]);
-    >> [x(1,1,1),x(2,1,1),x(1,2,1),x(1,1,2),x(4,5,6)]
-
-    ans =
-
-         1     2    11   111   594
-
-         */
-
-
-    std::cout << g(0,0,0) << " " << 1 << std::endl;
-    std::cout << g(1,0,0) << " " << 2 << std::endl;
-    std::cout << g(0,1,0) << " " << 11 << std::endl;
-    std::cout << g(0,0,1) << " " << 111 << std::endl;
-    std::cout << g(3,4,5) << " " << 594 << std::endl;
-
-}
 
 int main(int argc, char * argv[]) {
-    matlabTest();
-    testA();
-    testB();
+    test<2>();
+    test<3>();
 }
