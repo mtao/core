98e2feb8c7503986cce62b958d9ae6b89099fe21
Michael Tao <michael.tao@autodesk.com>
2019-01-26 19:37:25 -0500

    mean curvature stuff

--- include/mtao/geometry/mesh/laplacian.hpp
+++ include/mtao/geometry/mesh/laplacian.hpp
@@ -12,44 +12,43 @@ namespace mtao { namespace geometry { namespace mesh {
             std::vector<Triplet> trips;
             int mV = V.cols();
             Eigen::SparseMatrix<Scalar> L(mV,mV);
-            if(S.rows() == 3) {
-                auto OV = opposite_volumes(V,S);
-                std::vector<double> diag(mV,0);
-                for(int fi = 0; fi < S.cols(); ++fi) {
-                    auto&& le = OV.col(fi).array();
-                    auto&& f = S.col(fi);
+            assert(S.rows() == 3);
+            auto OV = opposite_volumes(V,S);
+            std::vector<double> diag(mV,0);
+            for(int fi = 0; fi < S.cols(); ++fi) {
+                auto&& le = OV.col(fi).array();
+                auto&& f = S.col(fi);
 
-                    double s = (le.sum()) / 2.0;
-                    double area = std::sqrt(s * (s-le).prod());
-                    double d = le.prod() / (2 * area);
+                double s = (le.sum()) / 2.0;
+                double area = std::sqrt(s * (s-le).prod());
+                double d = le.prod() / (2 * area);
 
-                    auto Sa = le/d;
+                auto Sa = le/d;
 
-                    for(int i = 0; i < 3; ++i) {
-                        double a = le((i+0)%3);
-                        double b = le((i+1)%3);
-                        double c = le((i+2)%3);
-                        double ca = ( b*b + c*c - a*a ) / ( 2 * b*c );
-                        double v = -.5 * ca / Sa(i);
+                for(int i = 0; i < 3; ++i) {
+                    double a = le((i+0)%3);
+                    double b = le((i+1)%3);
+                    double c = le((i+2)%3);
+                    double ca = ( b*b + c*c - a*a ) / ( 2 * b*c );
+                    double v = -.5 * ca / Sa(i);
 
 
-                        //int A = f((i+0)%3);
-                        int B = f((i+1)%3);
-                        int C = f((i+2)%3);
-                        trips.emplace_back(B,C,v);
-                        trips.emplace_back(C,B,v);
-                        diag[B] += -v;
-                        diag[C] += -v;
-                    }
-
+                    //int A = f((i+0)%3);
+                    int B = f((i+1)%3);
+                    int C = f((i+2)%3);
+                    trips.emplace_back(B,C,v);
+                    trips.emplace_back(C,B,v);
+                    diag[B] += -v;
+                    diag[C] += -v;
                 }
-                for(int i = 0; i < diag.size(); ++i) {
-                    trips.emplace_back(i,i,diag[i]);
-                }
-
-
 
             }
+            for(int i = 0; i < diag.size(); ++i) {
+                trips.emplace_back(i,i,diag[i]);
+            }
+
+
+
             L.setFromTriplets(trips.begin(),trips.end());
             return L;
         }
