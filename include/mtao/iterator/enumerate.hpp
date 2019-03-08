669b75beffd429e71c86ce9554e690814574cdc1
Michael Tao <mtao@dgp.toronto.edu>
2019-03-08 12:18:54 -0500

    making offset enumerates their own thing

--- include/mtao/iterator/enumerate.hpp
+++ include/mtao/iterator/enumerate.hpp
@@ -4,13 +4,21 @@
 
 namespace mtao {
     namespace iterator {
-    template <typename T>
-        auto enumerate(T&& v, int start=0) {
-            return zip(inf_range(start),std::forward<T>(v));
-        }
-    template <typename T>
-        auto enumerate(std::initializer_list<T>&& v,int start=0) {
-            return zip(inf_range(start),v);
-        }
-}
+        template <typename... T>
+            auto enumerate_offset(int start, std::initializer_list<T>&&... v) {
+                return zip(inf_range(start),v...);
+            }
+        template <typename... T>
+            auto enumerate_offset(int start, T&&... v) {
+                return zip(inf_range(start),std::forward<T>(v)...);
+            }
+        template <typename... T>
+            auto enumerate(T&&... v) {
+                return enumerate_offset(0,std::forward<T>(v)...);
+            }
+        template <typename... T>
+            auto enumerate(std::initializer_list<T>&&... v) {
+                return enumerate_offset(0,v...);
+            }
+    }
 }
