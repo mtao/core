7aebc513aad9a239f84bfb77d4ad2691d8c406f4
Michael Tao <mtao@dgp.toronto.edu>
2019-04-08 17:40:00 -0400

    hemisphere is broken, will fix tonight

--- include/mtao/geometry/mesh/hemisphere.hpp
+++ include/mtao/geometry/mesh/hemisphere.hpp
@@ -1,19 +1,21 @@
 #pragma once
-#include "hemisphere_ipl.hpp"
+#include "hemisphere_impl.hpp"
 
 
 namespace mtao { namespace geometry { namespace mesh {
 template <typename T, int D=3>
-    std::tuple<mtao::ColVectors<T,D>, mtao::ColVectors<int,D>> sphere(int depth) {
+    std::tuple<mtao::ColVectors<T,D>, mtao::ColVectors<int,D>> hemisphere(int depth) {
         if constexpr(D == 2) {//in 2d Depth is number of segments
             mtao::ColVectors<T,D> V(2,depth);
-            mtao::ColVectors<int,D> E(2,depth);
-            T pi2 = 2*std::acos(-1);
-            T dt = pi2 / depth;
+            mtao::ColVectors<int,D> E(2,depth-1);
+            T pi = std::acos(-1);
+            T dt = pi / depth;
             for(int i = 0; i < depth; ++i) {
                 //edges are "backwards" to match area_normals using right hand rule to allow the normals be outward facing
-                E(1,i) = i;
-                E(0,i) = (i+1)%depth;
+                if(i < depth-1) {
+                    E(1,i) = i;
+                    E(0,i) = (i+1)%depth;
+                }
                 V(0,i) = std::cos(i * dt);
                 V(1,i) = std::sin(i * dt);
             }
