ae46ea0aab4d10c8d51c811bc9c8954c2f30b3d3
Michael Tao <mtao@dgp.toronto.edu>
2019-08-31 21:50:31 -0400

    no working new functionality

--- include/mtao/geometry/mesh/write_ply.hpp
+++ include/mtao/geometry/mesh/write_ply.hpp
@@ -7,13 +7,13 @@
 namespace mtao::geometry::mesh {
 
     template <typename T>
-    write_ply(const mtao::ColVectors<T,3>& V, const mtao::ColVectors<T,3>& C,const mtao::ColVectors<int,3>& F, const std::string& filename);
+    void write_ply(const mtao::ColVectors<T,3>& V, const mtao::ColVectors<T,3>& C,const mtao::ColVectors<int,3>& F, const std::string& filename);
 
-    write_plyF(const mtao::ColVectors<float,3>& V,const mtao::ColVectors<int,3>& F, const std::string& filename);
-    write_plyD(const mtao::ColVectors<double,3>& V,const mtao::ColVectors<int,3>& F, const std::string& filename);
+    void write_plyF(const mtao::ColVectors<float,3>& V,const mtao::ColVectors<int,3>& F, const std::string& filename);
+    void write_plyD(const mtao::ColVectors<double,3>& V,const mtao::ColVectors<int,3>& F, const std::string& filename);
 
-    write_plyF(const mtao::ColVectors<float,3>& V, const mtao::ColVectors<float,3>& C, const mtao::ColVectors<int,3>& F, const std::string& filename);
-    write_plyD(const mtao::ColVectors<double,3>& V, const mtao::ColVectors<double,3>& C,const mtao::ColVectors<int,3>& F, const std::string& filename);
+    void write_plyF(const mtao::ColVectors<float,3>& V, const mtao::ColVectors<float,3>& C, const mtao::ColVectors<int,3>& F, const std::string& filename);
+    void write_plyD(const mtao::ColVectors<double,3>& V, const mtao::ColVectors<double,3>& C,const mtao::ColVectors<int,3>& F, const std::string& filename);
 }
 
 #endif//READ_PLY_H
