43a2ff27aee94527c812c9911f1a8fb4163f8e64
Michael Tao <mtao@dgp.toronto.edu>
2019-09-17 16:32:08 -0400

    mesh info should work but segfaults

---
+++ tools/mesh_info.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <mtao/geometry/mesh/read_obj.hpp>
+#include <mtao/geometry/bounding_box.hpp>
+
+int main(int argc, char * argv[]) {
+    std::string filename = argv[1];
+    auto [V,F] = mtao::geometry::mesh::read_objD(filename);
+    std::cout << "#V,#F: " << V.cols() << "," << F.cols() << std::endl;
+    auto bb = mtao::geometry::bounding_box(V);
+    std::cout << "Bounding box: " << bb.min().transpose() << " => " << bb.max().transpose() << std::endl;
+}
