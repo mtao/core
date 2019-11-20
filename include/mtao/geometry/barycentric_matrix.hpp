bfc6bceaa0c6d875afface528ab287733a073906
Michael Tao <michael@augmenta.ai>
2019-11-19 21:14:02 -0500

    adding PC constant `barycentric` interpolation

--- include/mtao/geometry/barycentric_matrix.hpp
+++ include/mtao/geometry/barycentric_matrix.hpp
@@ -4,9 +4,11 @@
 
 
 namespace mtao::geometry {
+    //A collection of simplicies, indices into simplices, and related barycentric coordinates
     template <typename SimplexType, typename BType, typename SIType>
         Eigen::SparseMatrix<typename BType::Scalar> 
         barycentric_matrix(int num_vertices, const Eigen::MatrixBase<SimplexType>& S, const Eigen::MatrixBase<SIType>& SI, const Eigen::MatrixBase<BType>& B) {
+            assert(SI.size() == B.cols());
 
 
             using Scalar = typename BType::Scalar; 
@@ -23,6 +25,23 @@ namespace mtao::geometry {
                 {
                     trips.emplace_back(i,s(j),b(j));
                 }
+            }
+            A.setFromTriplets(trips.begin(),trips.end());
+            return A;
+        }
+    //A collection of simplicies, indices into simplices, and related barycentric coordinates
+    template <typename Scalar, typename SIType>
+        Eigen::SparseMatrix<Scalar> 
+        barycentric_matrix_face(int num_faces, const Eigen::MatrixBase<SIType>& SI) {
+
+            Eigen::SparseMatrix<Scalar> A(SI.rows(), num_faces);
+
+            std::vector<Eigen::Triplet<Scalar>> trips;
+            trips.reserve(SI.size());
+
+            for(int i = 0; i < SI.cols(); ++i)
+            {
+                trips.emplace_back(i,SI(i),1);
             }
             A.setFromTriplets(trips.begin(),trips.end());
             return A;
