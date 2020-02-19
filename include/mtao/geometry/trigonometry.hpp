aeb53b62fbca8fcb5cf2b785d632606972e3c36b
Michael Tao <mtao@dgp.toronto.edu>
2020-02-19 14:33:53 -0500

    fixing a mysterious issue noticed on osx with binaryexpr

--- include/mtao/geometry/trigonometry.hpp
+++ include/mtao/geometry/trigonometry.hpp
@@ -38,7 +38,15 @@ namespace mtao::geometry::trigonometry {
             using S = typename Derived::Scalar;
             Eigen::Matrix<S,1,Derived::ColsAtCompileTime> A(1,P.cols());
 
+#ifdef TODO_CHECK_WHEN_THIS_IS_OK
             A = P.row(0).binaryExpr(P.row(1), std::function(angle_scalar<S>));
+#else
+            auto a = P.row(0);
+            auto b = P.row(1);
+            for(int i = 0; i < P.cols(); ++i) {
+                A(i) = angle_scalar<S>(a(i),b(i));
+            }
+#endif
             return A;
         }
 
