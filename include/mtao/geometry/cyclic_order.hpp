ab22b512470a81eaad53af8d0d1649632be6291a
Michael Tao <mtao@dgp.toronto.edu>
2020-03-04 22:45:20 -0500

    adding an efficient cyclic ordering tool

---
+++ include/mtao/geometry/cyclic_order.hpp
@@ -0,0 +1,60 @@
+#pragma once
+#include <array>
+#include <numeric>
+#include <vector>
+
+#include "mtao/types.hpp"
+#include "mtao/iterator/enumerate.hpp"
+
+namespace mtao::geometry {
+constexpr static std::array<int, 4> __quadrants{{4, 1, 3, 2}};
+template <typename T>
+std::vector<int> cyclic_order(const mtao::ColVectors<T, 2>& V) {
+    size_t size = V.cols();
+    // sort indices by quadrant and cross product
+    std::vector<char> quadrants(size);
+    for (size_t i = 0; i < size; ++i) {
+        auto b = V.col(i);
+        // ++ +- -+ -- => 1 4 2 3
+        quadrants[i] =
+            __quadrants[2 * std::signbit(b.y()) + std::signbit(b.x())];
+    }
+    // sort by quadrant and then by cross product volume
+    auto comp = [&](int ai, int bi) -> bool {
+        const char qa = quadrants[ai];
+        const char qb = quadrants[bi];
+        if (qa == qb) {
+            auto a = V.col(ai);
+            auto b = V.col(bi);
+            return b.x() * a.y() < a.x() * b.y();
+        } else {
+            return qa < qb;
+        }
+    };
+    // we need to sort D and quadrants simultaneously, easier to just sort
+    // indices into both.
+    std::vector<int> ordered_indices(size);
+    // spit the initial indices of indices configuration
+    std::iota(ordered_indices.begin(), ordered_indices.end(), 0);
+    // sort the indices of indices
+    std::sort(ordered_indices.begin(), ordered_indices.end(), comp);
+    return ordered_indices;
+}
+
+template <typename T>
+std::vector<int> cyclic_order(const mtao::ColVectors<T, 2>& V, int base,
+                              const std::vector<int>& indices) {
+    const auto o = V.col(base);
+    mtao::ColVecs2d D(2, indices.size());
+    for (auto [i, j] : mtao::iterator::enumerate(indices)) {
+        D.col(i) = V.col(j) - o;
+    }
+    std::vector<int> ordered_indices = cyclic_order(D);
+
+    // dereference the indices of indices
+    std::transform(ordered_indices.begin(), ordered_indices.end(),
+                   ordered_indices.begin(),
+                   [&](int idx) -> int { return indices[idx]; });
+    return ordered_indices;
+}
+}  // namespace mtao::geometry
---
+++ tests/cyclic_order_test.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <mtao/geometry/cyclic_order.hpp>
+#include <catch2/catch.hpp>
+#include <iterator>
+#include <iostream>
+#include <algorithm>
+
+using E = std::array<int, 2>;
+
+bool is_cyclic_ordered(const std::vector<int>& indices) {
+
+
+    for(size_t idx = 0; idx < indices.size(); ++idx) {
+        if( int((indices[idx] +1 )%indices.size()) != indices[(idx+1)%indices.size()]) {
+            return false;
+        }
+    }
+
+    return true;
+
+}
+
+TEST_CASE("Polygon", "[cyclic_order]") {
+    for (int N = 3; N < 50; ++N) {
+        std::stringstream ss;
+        ss << "Polygon size " << N;
+        SECTION(ss.str());
+        mtao::ColVecs2d V(2, N);
+
+        mtao::VecXd ang = mtao::VecXd::LinSpaced(N, 0, 2 * M_PI * (N - 1.) / N);
+        V.row(0) = ang.array().cos().transpose();
+        V.row(1) = ang.array().sin().transpose();
+
+
+        auto order = mtao::geometry::cyclic_order(V);
+        REQUIRE( is_cyclic_ordered(order) );
+    }
+}
+TEST_CASE("Polygon with Center", "[cyclic_order]") {
+    for (int N = 3; N < 50; ++N) {
+        std::stringstream ss;
+        ss << "Polygon size " << N;
+        SECTION(ss.str());
+        mtao::ColVecs2d V(2, N);
+
+        mtao::VecXd ang = mtao::VecXd::LinSpaced(N, 0, 2 * M_PI * (N - 1.) / N);
+        V.row(0).head(N) = ang.array().cos().transpose();
+        V.row(1).head(N) = ang.array().sin().transpose();
+
+
+        auto order = mtao::geometry::cyclic_order(V);
+        REQUIRE( is_cyclic_ordered(order) );
+
+    }
+
+}
+TEST_CASE("Near Axis Fan", "[cyclic_order]") {
+    for (int N = 3; N < 50; ++N) {
+        std::stringstream ss;
+        SECTION(ss.str());
+        mtao::ColVecs2d V(2, N);
+
+        V.row(0).head(N).setConstant(1e-8);
+        V.row(1).head(N) = mtao::VecXd::LinSpaced(N, 1, 1e10);
+
+        auto order = mtao::geometry::cyclic_order(V);
+        REQUIRE( is_cyclic_ordered(order) );
+
+    }
+}
+
+TEST_CASE("Near Diagonal Fan", "[cyclic_order]") {
+    for (int N = 3; N < 50; ++N) {
+        std::stringstream ss;
+        SECTION(ss.str());
+        mtao::ColVecs2d V(2, N);
+
+        V.row(0).head(N) = mtao::VecXd::LinSpaced(N, 1, 1e10);
+        V.row(1).head(N) = V.row(0).head(N).array() - .5;
+
+        auto order = mtao::geometry::cyclic_order(V);
+        REQUIRE( is_cyclic_ordered(order) );
+
+
+    }
+}
+
