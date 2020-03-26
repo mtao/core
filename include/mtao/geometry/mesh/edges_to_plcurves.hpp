714311e432044e95f0a2df0f0cb1d4b642f564d6
Michael Tao <mtao@dgp.toronto.edu>
2020-03-26 01:04:21 -0400

    added something to create boundary loops from an unstructured set of edges

---
+++ include/mtao/geometry/mesh/edges_to_plcurves.hpp
@@ -0,0 +1,113 @@
+#pragma once
+#include <set>
+#include <tuple>
+#include <vector>
+
+#include "mtao/types.hpp"
+
+namespace mtao::geometry::mesh {
+// converts a collection of curves represented as pairs of indices in E into a
+// collection of curves. By default it alwys generates closed curves but when
+// closed_only=false it also can generate open and closed curves as long as
+// vertices have valence <=2 This function returns a collection of <vector,bool>
+// pairs where hte bool says whehter the returned curve is closed. if
+// closed_only=true then the bool is always true
+template <typename EDerived>
+std::vector<std::tuple<std::vector<int>, bool>> edge_to_plcurves(
+    const Eigen::MatrixBase<EDerived>& E, bool closed_only = true) {
+    std::map<int, std::array<int, 2>> adj;
+    for (int i = 0; i < E.cols(); ++i) {
+        auto e = E.col(i);
+        if (auto it = adj.find(e(0)); it == adj.end()) {
+            adj[e(0)] = std::array<int, 2>{{i, -1}};
+        } else {
+            // it->second[1] = i;
+            adj[e(0)][1] = i;
+        }
+        if (auto it = adj.find(e(1)); it == adj.end()) {
+            adj[e(1)] = std::array<int, 2>{{i, -1}};
+        } else {
+            // it->second[0] = i;
+            adj[e(1)][1] = i;
+        }
+    }
+
+    // create an orientation by running around open edges and then random edges
+    std::set<int> unseen_edges;
+    std::set<int> open_edges;
+    for (auto&& [k, v] : adj) {
+        unseen_edges.emplace(k);
+        if (v[0] < 0 || v[1] < 0) {
+            open_edges.emplace(k);
+        }
+    }
+    auto remove = [&](int idx) {
+        if (auto it = open_edges.find(idx); it != open_edges.end()) {
+            open_edges.erase(it);
+        }
+        if (auto it = unseen_edges.find(idx); it != unseen_edges.end()) {
+            unseen_edges.erase(it);
+        }
+    };
+    std::vector<std::tuple<std::vector<int>, bool>> ret;
+    auto run = [&](const int start_vertex, const int start_edge) {
+        std::cout << "Start!" << std::endl;
+        auto& [vec, closedness] = ret.emplace_back();
+        closedness = true;
+        int vertex = start_vertex;
+        int edge = start_edge;
+        do {
+            std::cout << vertex << ":" << edge << std::endl;
+            vec.emplace_back(vertex);
+            auto&& e = E.col(edge);
+            // step vertex
+            if (e[0] == vertex) {
+                vertex = e[1];
+            } else {
+                vertex = e[0];
+            }
+            remove(vertex);
+
+            // step edge and swap it into the right order
+            auto& de = adj[vertex];
+            if (de[0] != edge) {
+                std::swap(de[0], de[1]);
+            }
+            edge = de[1];
+            // if we hit an end we can continue wrapping around or end,
+            // depending on closed_only
+            if (edge < 0) {
+                closedness = false;
+                if (closed_only) {
+                    edge = de[0];
+                } else {
+                    break;
+                }
+            }
+
+        } while (vertex != start_vertex);
+    };
+    while (!open_edges.empty()) {
+        int start_idx = *open_edges.begin();
+        remove(start_idx);
+        int start_edge;
+        auto&& e = adj[start_idx];
+        if (e[0] == -1) {
+            start_edge = e[1];
+        } else {
+            start_edge = e[0];
+        }
+        std::cout << e[0] << "::" << e[1] << std::endl;
+        run(start_idx, start_edge);
+    }
+    while (!unseen_edges.empty()) {
+        int start_idx = *unseen_edges.begin();
+        remove(start_idx);
+        auto&& e = adj[start_idx];
+        int start_edge = e[0];
+
+        run(start_idx, start_edge);
+    }
+    return ret;
+}  // namespace mtao::geometry::mesh
+}  // namespace mtao::geometry::mesh
---
+++ tests/edges_to_plcurves_test.cpp
@@ -0,0 +1,76 @@
+#include <algorithm>
+#include <catch2/catch.hpp>
+#include <iostream>
+#include <iterator>
+#include <mtao/geometry/mesh/edges_to_plcurves.hpp>
+
+using namespace mtao::geometry::mesh;
+
+TEST_CASE("ClosedCurves", "[edges_to_plcurves]") {
+    auto process = [](const mtao::ColVecs2i& E) {
+        std::cout << E << std::endl;
+        auto r = edge_to_plcurves(E);
+        for (auto&& [vec, closedness] : r) {
+            std::copy(vec.begin(), vec.end(),
+                      std::ostream_iterator<int>(std::cout, ","));
+            REQUIRE(vec.size() == E.cols());
+            REQUIRE(closedness == true);
+            if (closedness) {
+                std::cout << "closed" << std::endl;
+            } else {
+                std::cout << "open" << std::endl;
+            }
+        }
+    };
+    {
+        mtao::ColVecs2i E(2, 3);
+        E.col(0) << 0, 1;
+        E.col(1) << 1, 2;
+        E.col(2) << 2, 0;
+        process(E);
+    }
+    {
+        mtao::ColVecs2i E(2, 4);
+        E.col(0) << 0, 1;
+        E.col(1) << 1, 2;
+        E.col(2) << 2, 3;
+        E.col(3) << 3, 0;
+        process(E);
+    }
+}
+TEST_CASE("OpenCurves", "[edges_to_plcurves]") {
+    auto process = [](const mtao::ColVecs2i& E) {
+        std::cout << E << std::endl;
+        auto r = edge_to_plcurves(E);
+        for (auto&& [vec, closedness] : r) {
+            std::copy(vec.begin(), vec.end(),
+                      std::ostream_iterator<int>(std::cout, ","));
+            REQUIRE(closedness == false);
+            REQUIRE(vec.size() == 2 * E.cols());
+            if (closedness) {
+                std::cout << "closed" << std::endl;
+            } else {
+                std::cout << "open" << std::endl;
+            }
+        }
+    };
+    {
+        mtao::ColVecs2i E(2, 1);
+        E.col(0) << 0, 1;
+        process(E);
+    }
+    {
+        mtao::ColVecs2i E(2, 2);
+        E.col(0) << 0, 1;
+        E.col(1) << 1, 2;
+        process(E);
+    }
+    {
+        mtao::ColVecs2i E(2, 4);
+        E.col(0) << 0, 1;
+        E.col(1) << 1, 2;
+        E.col(2) << 2, 3;
+        E.col(3) << 3, 4;
+        process(E);
+    }
+}
