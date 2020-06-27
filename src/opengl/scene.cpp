394740bb5bd56354d2f258a192791918a7c70031
Michael Tao <mtao@dgp.toronto.edu>
2020-06-26 21:25:41 -0400

    scene update/

--- src/opengl/scene.cpp
+++ src/opengl/scene.cpp
@@ -1,16 +1,28 @@
 #include "mtao/opengl/scene.h"
 
 namespace mtao::opengl {
-Scene3D::Scene3D() {
-    _camera->setParent(&_scene).translate(Vector3::zAxis(5.0f));
-    _camera.setAspectRatioPolicy(SceneGraph::AspectRatioPolicy::Extend)
-        .setProjectionMatrix(
-            Matrix4::perspectiveProjection(35.0_degf, 1.0f, 0.01f, 1000.0f))
-        .setViewport(GL::defaultFramebuffer.viewport().size());
 
-    _root.setParent(&_scene);
-}
-void Scene3D::setViewport(const Magnum::Vector2i& windowSize) {
-    _camera.setViewport(windowSize);
-}
+    void Scene2D::drawGroup(const SceneGraph::DrawableGroup2D& group) {
+        activeCamera().draw(group);
+    }
+
+    void Scene3D::drawGroup(const SceneGraph::DrawableGroup3D& group) {
+        activeCamera().draw(group);
+    }
+    void Scene2D::drawGroups() {
+        for(auto&& [a,pr]: _draw_groups) {
+            auto&& [group, visible] = pr;
+            if(visible) {
+                drawGroup(group);
+            }
+        }
+    }
+    void Scene3D::drawGroups() {
+        for(auto&& [a,pr]: _draw_groups) {
+            auto&& [group, visible] = pr;
+            if(visible) {
+                drawGroup(group);
+            }
+        }
+    }
 }  // namespace mtao::opengl
