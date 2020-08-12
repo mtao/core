baf6af244a6ff2848ad2f17a7e7438d79373a053
Michael Tao <mtao@dgp.toronto.edu>
2020-08-12 18:29:10 -0400

    updating eigen shape checks to not assert by default, updated centroid and volume for triangle/tet

--- include/mtao/geometry/centroid.hpp
+++ include/mtao/geometry/centroid.hpp
@@ -50,19 +50,16 @@ auto curve_centroid(const Eigen::MatrixBase<Derived>& V,
                     const Eigen::MatrixBase<EDerived>& E,
                     const std::map<int, bool>& C) {
     for (auto&& [eidx, sgn] : C) {
-        for (; it != endit; ++it, ++it1) {
-            if (it1 == endit) {
-                it1 = beginit;
-            }
-            auto a = V.col(*it);
-            auto b = V.col(*it1);
-            auto v = triangle_area(a, b);
-            ret += v * (a + b) / 3;
-            vol += v;
-        }
-        if (vol > 1e-10) {
-            ret /= vol;
-        }
+        auto e = E.col(eidx);
+        auto a = V.col(e(0));
+        auto b = V.col(e(1));
+        auto v = triangle_area(a, b);
+        ret += v * (a + b) / 3;
+        vol += v;
     }
+    if (vol > 1e-10) {
+        ret /= vol;
+    }
+}
 }  // namespace mtao::geometry
 #endif  // CENTROID_HPP
