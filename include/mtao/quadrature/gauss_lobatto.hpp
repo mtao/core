5c4a5721506046fe8a61820facbd2df51bc25419
Michael Tao <mtao@dgp.toronto.edu>
2020-09-19 13:23:10 -0400

    allowing for gauss lobatto to work at lower orders

--- include/mtao/quadrature/gauss_lobatto.hpp
+++ include/mtao/quadrature/gauss_lobatto.hpp
@@ -12,15 +12,19 @@ gauss_lobatto_data(int n = 3) {
     const static T s5 = T(1) / std::sqrt<T>(5).real();
     const static T s37 = std::sqrt<T>(T(3) / 7).real();
     const static std::vector<std::vector<T>> points{
+        {T(0)},
+        {T(-1), T(1)},
         {T(-1), T(0), T(1)},
         {T(-1), -s5, s5, T(1)},
         {T(-1), -s37, T(0), s37, T(1)}};
     const static std::vector<std::vector<T>> weights{
+        {T(1)},
+        {T(1) / 2, T(1) / 2},
         {T(1) / 3, T(4) / 3, T(1) / 3},
         {T(1) / 6, T(5) / 6, T(5) / 6, T(1) / 6},
         {T(1) / 10, T(49) / 90, T(32) / 45, T(49) / 90, T(1) / 10}};
 
-    return {points.at(n - 3), weights.at(n - 3)};
+    return {points.at(n - 1), weights.at(n - 1)};
 }
 
 template <typename Scalar>
