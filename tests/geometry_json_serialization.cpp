b359165ace289a7efefa3eb9f924a255bf19c80b
Michael Tao <mtao@dgp.toronto.edu>
2021-01-07 20:00:34 -0500

    adding something to make serializing bounding boxes to json a bit easier

---
+++ tests/geometry_json_serialization.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <mtao/json/bounding_box.hpp>
+
+
+int main(int argc, char *argv[]) {
+    Eigen::AlignedBox<double, 2> bbox;
+    bbox.min().setZero();
+    bbox.max().setOnes();
+
+    nlohmann::json js;
+    js["bounding_box"] = bbox;
+
+
+    auto bb = js["bounding_box"].get<Eigen::AlignedBox<double, 2>>();
+    std::cout << bb.min().transpose() << std::endl;
+    std::cout << bb.max().transpose() << std::endl;
+
+
+    js["Normal"] = Eigen::Vector3d::UnitZ();
+
+
+    std::cout << js.dump(2) << std::endl;
+    //std::cout << js["Normal"].get<Eigen::Vector3d>();
+}
